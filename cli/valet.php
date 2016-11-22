af306c6f48c814c0bb5b946ee47a236632272b76
Joaquin Marcher <joaquin@marcher.com.uy>
2016-11-22 22:54:53 +0100

    Bump version

--- cli/valet.php
+++ cli/valet.php
@@ -18,7 +18,7 @@ use Silly\Application;
  */
 Container::setInstance(new Container());
 
-$version = '2.0.10';
+$version = '2.1.0';
 
 $app = new Application('Laravel Valet (Linux Edition)', $version);
 
