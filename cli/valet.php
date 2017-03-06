baa7fec6ffe5869645dead9f2479ab8c94450793
Joaquin Marcher <joaquin@marcher.com.uy>
2017-03-06 16:34:59 +0100

    Update valet.php

--- cli/valet.php
+++ cli/valet.php
@@ -18,7 +18,7 @@ use Silly\Application;
  */
 Container::setInstance(new Container());
 
-$version = '2.2.2';
+$version = '2.2.3';
 
 $app = new Application('Laravel Valet (Linux Edition)', $version);
 
